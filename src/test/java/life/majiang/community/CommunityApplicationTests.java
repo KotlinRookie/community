8c1f77f0de820a9bef4734f434d7e9ad5e4ef781
Jou <jou@JoudeMacBook-Pro.local>
2023-02-23 18:30:46 +0900

    test1

--- src/test/java/life/majiang/community/CommunityApplicationTests.java
+++ src/test/java/life/majiang/community/CommunityApplicationTests.java
@@ -1,13 +1,24 @@
 package life.majiang.community;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 
 @SpringBootTest
-class CommunityApplicationTests {
+public class CommunityApplicationTests {
+
+    @Autowired(required = false)
+    private DataSource dataSource;
 
     @Test
-    void contextLoads() {
+    public void aaa() throws SQLException {
+        System.out.println(dataSource.getConnection());
     }
 
 }
